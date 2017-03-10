fdcd1168441af3f8707de0946f79c7fb861baef4
Håvard Aspen Løvik <hloevi@live.no>
2017-03-10 12:22:39 +0100

    made mysql connection work

--- treningsdagbok/src/treningsdagbok/Main.java
+++ treningsdagbok/src/treningsdagbok/Main.java
@@ -1,9 +1,34 @@
 package treningsdagbok;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-public class Main {
+// Notice, do not import com.mysql.jdbc.*
+// or you will have problems!
 
-	public static void main(String[] args) {
-		System.out.println("hello world");
-	}
+public class Main{
+    public static void main(String[] args) {
+        try {
+            // The newInstance() call is a work around for some
+            // broken Java implementations
+
+            Class.forName("com.mysql.jdbc.Driver").newInstance();
+        } catch (Exception ex) {
+            // handle the error
+        	System.out.println(ex);
+        }
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://localhost/treningsdagbok?" +
+                                           "user=root&password=root");
+
+            // Do something with the Connection
+
+        } catch (SQLException ex) {
+            // handle any errors
+            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLState: " + ex.getSQLState());
+            System.out.println("VendorError: " + ex.getErrorCode());
+        }
+    }
 }
-
