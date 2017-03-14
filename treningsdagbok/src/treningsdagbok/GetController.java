a5f9ea3a72f70932eb72f00acc56e3956bd43e1b
oab8 <oyvindab8@gmail.com>
2017-03-14 16:30:15 +0100

    Update GetController.java

--- treningsdagbok/src/treningsdagbok/GetController.java
+++ treningsdagbok/src/treningsdagbok/GetController.java
@@ -1,5 +1,29 @@
 package treningsdagbok;
 
-public class GetController {
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+public class GetQuery {
+	public void send(Connection con, String query) throws SQLException{
+		Statement stmt = null;
+		try {
+			stmt = con.createStatement();
+	        ResultSet rs = stmt.executeQuery(query);
+	        while (rs.next()) {
+				int prestasjon = rs.getInt("Prestasjon");
+				System.out.print(prestasjon);
+	        }
+		}
+		catch (SQLException e ) {
+	        System.out.println(e);
+	    	System.out.println("Statement was not executed");
+	    } 
+		finally {
+	        stmt.close();
+	        }
+		
+		
+	}
 }
