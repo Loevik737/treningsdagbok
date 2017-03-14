aec3e170499f47773b6721b4538b47a2532e5b03
Edvin Johnsrud <edvin.johnsrud@gmail.com>
2017-03-14 15:50:15 +0100

    finished one of the functions

--- treningsdagbok/src/treningsdagbok/Treningsokt.java
+++ treningsdagbok/src/treningsdagbok/Treningsokt.java
@@ -6,8 +6,8 @@ import java.time.LocalDateTime;
 public class Treningsokt {
 	
 	public void newTreningsokt(Connection con,LocalDateTime start, LocalDateTime end,String note, String name,Integer form, Boolean mal){
-		//skriv dette om til å ta in parameterene over    
-		String query = "INSERT INTO trenign (Luft,Tilskuere)" +
+		//rewrite this to take in the parameters above
+		String query = "INSERT INTO trening (Luft,Tilskuere)" +
 		    			   "VALUES (200, 123)";
 		}
 }
--- treningsdagbok/src/treningsdagbok/userfullOperations.java
+++ treningsdagbok/src/treningsdagbok/userfullOperations.java
@@ -2,15 +2,34 @@ package treningsdagbok;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Date;
+import java.util.Scanner;
 /**
  * Created by edvinj on 14.03.17.
  */
 public class userfullOperations {
 
     public utilityCall(String keyword) throws SQLException {
-        if(keyword == "sort") {
-
+        Connect connection = new Connect();
+        Connection conn = connection.getConnection();
+        boolean cont = true;
+        Scanner regWorkOut = new Scanner(System.in);
+        while (cont) {
+            System.out.println("Enter your action");
+            String action = regWorkOut.nextLine();
+            if (action.equals("ny")) {
+                System.out.println("Enter your workout using this format: Date(YYYY.MM.DD); Startime(hh:mm:ss); Stoptime(hh:mm:ss); General notes; Name; Form; Template(0 , 1)");
+                String result = regWorkOut.nextLine();
+                String[] data = result.split(";");
+                String starttime = data[0] + " " + data[1];
+                String stoptime = data[0] + " " + data[2];
+                String notes =  data[3];
+                String name = data[4];
+                String form = data[5];
+                String template = data[6];
+                Treningsokt trening = new Treningsokt();
+                trening.newTreningsokt(conn, starttime, stoptime, notes, name, form, template);
+            }
         }
-        return query;
     }
 }
