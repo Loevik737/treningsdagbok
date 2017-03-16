0c6098c0113d7eb0454a56ab7145f8d3266bcfc1
Edvin Johnsrud <edvin.johnsrud@gmail.com>
2017-03-16 15:26:06 +0100

    added ability to input more data

--- treningsdagbok/src/treningsdagbok/userfullOperations.java
+++ treningsdagbok/src/treningsdagbok/userfullOperations.java
@@ -8,8 +8,8 @@ import java.util.Scanner;
 public class userfullOperations {
 
     public void utilityCall() throws SQLException {
-        String allActions = "List of all possible commands:" + '\n' + "new: Adds a new workout " + '\n' + "stop: Stops the program " +
-                '\n' + "results: returns your results " + '\n' + "best: returns your best result";
+        String allActions = "List of all possible commands:" + '\n' + "new workout: Adds a new workout " + '\n' + "new result: Adds a new result "
+                + '\n' + "stop: Stops the program " + '\n' + "results: returns your results " + '\n' + "best: returns your best result";
         Connect connection = new Connect();
         Connection conn = connection.getConnection();
         boolean cont = true;
@@ -17,8 +17,8 @@ public class userfullOperations {
         while (cont) {
             System.out.println("Enter your action");
             String action = regWorkOut.nextLine();
-            if (action.equals("new")) {
-                System.out.println("Enter your workout using this format: Date(YYYY.MM.DD); Startime(hh:mm:ss); Stoptime(hh:mm:ss); General notes; Name; Form; Template(0 , 1)");
+            if (action.equals("new workout")) {
+                System.out.println("Enter your workout using this format: Date(YYYY.MM.DD); Startime(hh:mm); Stoptime(hh:mm); General notes; Name; Form; Template(0 , 1)  ");
                 String result = regWorkOut.nextLine();
                 String[] data = result.split(";");
                 String starttime = data[0] + " " + data[1];
@@ -29,6 +29,23 @@ public class userfullOperations {
                 String template = data[6];
                 Treningsokt trening = new Treningsokt();
                 trening.newTreningsokt(conn, starttime, stoptime, notes, name, form, template);
+                System.out.println("Enter Achievement between 0 and 99;"  + "Description of result; Where you workout found place: (indoor/outdoor);"  + "If you selected indoor, then enter airquality; and crowd size;, if you selected outdoor, enter 'weather'; and 'temperature'; ");
+                String info = regWorkOut.nextLine();
+                String[] followup = info.split(";");
+                String achievement = followup[0];
+                String description = followup[1];
+                String condition1 = followup[3];
+                String condition2 = followup[4];
+                Treningsokt resultat = new Treningsokt();
+                resultat.newResult(conn, achievement, description, name);
+                if (data[9].equals(" outdoor")) {
+                    Treningsokt outside = new Treningsokt();
+                    outside.newOutoorCondition(conn, condition1, condition2, name);
+                }
+                else {
+                    Treningsokt inside = new Treningsokt();
+                    inside.newIndoorCondition(conn, condition1, condition2, name);
+                }
             }
             else if (action.equals("stop")) {
                 cont = false;
@@ -40,7 +57,7 @@ public class userfullOperations {
             else if (action.equals("best")) {
                 GetController result = new GetController();
                 for (Integer i: result.send(conn, "SELECT MAX(Prestasjon) FROM resultat")) {
-                    System.out.println(i);
+                    System.out.println(i + " out of maximum 99.");
                 }
             }
             else {
